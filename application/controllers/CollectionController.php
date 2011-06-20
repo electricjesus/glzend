3937d1299f62342aa68288df76e5604f4a156dbb
Seth Malaki <seth.malaki@gmail.com>
2011-06-20 17:48:46 +0800

    Changed layout.. moved flowplayer JS

--- application/controllers/CollectionController.php
+++ application/controllers/CollectionController.php
@@ -4,7 +4,7 @@ class CollectionController extends Zend_Controller_Action
 {
     public function init()
     {
-        /* Initialize action controller here */
+        /* Initialize action controller here */        
     }
     public function indexAction() { 
         $this->_forward('latevictorian');
