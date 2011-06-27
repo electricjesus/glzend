646e24e3a66ff4fa61444c57b3d17e94f82daf2e
Seth Malaki <seth.malaki@gmail.com>
2011-06-27 17:48:18 +0800

    Built collection actions to have a common function to call to build data

--- application/controllers/CollectionController.php
+++ application/controllers/CollectionController.php
@@ -11,27 +11,34 @@ class CollectionController extends Zend_Controller_Action
      }
     public function latevictorianAction() { 
         // construct data
-        $_category_name = "late-victorian";
-        $this->view->assign('categoryname', $_category_name);
+        $this->_buildCategoryData('3', "late-victorian");
         $this->render('collection'); 
     }
     public function artnouveauAction() { 
-        $_category_name = "art-nouveau";
-        $this->view->assign('categoryname', $_category_name);
+        $this->_buildCategoryData('2', "art-nouveau");
         $this->render('collection');
     }
     public function artdecoAction() { 
+        $this->_buildCategoryData('1',"art-deco");
+        $this->render('collection');
+    }
+    public function aAction() {
+        $this->view->layout()->disableLayout();
+        $this->view->assign('mode', $this->_getParam('m'));     // product or subcategory
+        $this->view->assign('id', $this->_getParam('id'));      // the ID
+        $this->view->assign('where', $this->_getParam('w'));    // carousel or slideshow
+        $this->view->assign('category', $this->_getParam('c')); // category
+    }
+    protected function _buildCategoryData ($_category_id, $_category_name) {
         $_products = new Databases_Products;
         $_subcategories = new Databases_Designers;
-        $_category_id = 1;
-        $_category_name = "art-deco";
         //construct data for slideshow
         $_slideshowdata = 
             $_products->fetchAll(
                 $_products
                 ->select(Zend_Db_Table::SELECT_WITH_FROM_PART)->setIntegrityCheck(false)
                 ->join('featured','featured.product_id = products.id')
-                ->where('featured.location = ?','category')->where('category_id = ?',$_category_id )
+                ->where('featured.location = ?','category')->where('category_id = ?', $_category_id )
             );
         //construct data for carousel
         $_carouseldata = 
@@ -41,25 +48,17 @@ class CollectionController extends Zend_Controller_Action
                     ->join('sort','sort.item_id = products.id')
                     ->where('sort.item_type = ?','product')->where('category_id = ?', $_category_id )
             );
-        $_designerdata = 
-            $_products->fetchAll(
-                $_products
-                    ->select(Zend_Db_Table::SELECT_WITH_FROM_PART)->setIntegrityCheck(false)
-                    ->join('sort','sort.item_id = products.id')
-                    ->where('sort.item_type = ?','product')->where('category_id = ?', $_category_id )
+        $_designerdata = $_subcategories->fetchAll(
+                $_subcategories
+                    ->select()->from(array('subcategories'),array('designerid'=>'id','name','category_id','description'))
+                    ->setIntegrityCheck(false)->join('sort','sort.item_id = subcategories.id')
+                    ->where('sort.item_type = ?','designer')->where('category_id = ?', $_category_id)
+                    ->order('sort_order ASC')
                     );
         $this->view->assign('slideshowdata', $_slideshowdata);
         $this->view->assign('carouseldata', $_carouseldata);
         $this->view->assign('designerdata', $_designerdata);
         $this->view->assign('categoryname', $_category_name);
-        $this->render('collection');
-    }
-    public function aAction() {
-        $this->view->layout()->disableLayout();
-        $this->view->assign('mode', $this->_getParam('m'));     // product or subcategory
-        $this->view->assign('id', $this->_getParam('id'));      // the ID
-        $this->view->assign('where', $this->_getParam('w'));    // carousel or slideshow
-        $this->view->assign('category', $this->_getParam('c')); // category
     }
 }
 
