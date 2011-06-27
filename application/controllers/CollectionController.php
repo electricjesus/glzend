302b47e9251e1faef199022562dc9c67dd4f98a4
Seth Malaki <seth.malaki@gmail.com>
2011-06-27 16:15:10 +0800

    Added new stuff for the new design.. changed collection routes to use only one view even with multiple controller actions.

--- application/controllers/CollectionController.php
+++ application/controllers/CollectionController.php
@@ -9,9 +9,45 @@ class CollectionController extends Zend_Controller_Action
     public function indexAction() { 
         $this->_forward('latevictorian');
      }
-    public function latevictorianAction() { /*action body*/ }
-    public function artnouveauAction() { /*action body*/ }
-    public function artdecoAction() { /*action body*/ }
+    public function latevictorianAction() { 
+        // construct data
+        $this->render('collection'); 
+    }
+    public function artnouveauAction() { 
+        $this->render('collection');
+    }
+    public function artdecoAction() { 
+        $_products = new Databases_Products;
+        $_subcategories = new Databases_Designers;
+        $_category_id = 1;
+        //construct data for slideshow
+        $_slideshowdata = 
+            $_products->fetchAll(
+                $_products
+                ->select(Zend_Db_Table::SELECT_WITH_FROM_PART)->setIntegrityCheck(false)
+                ->join('featured','featured.product_id = products.id')
+                ->where('featured.location = ?','category')->where('category_id = ?',$_category_id )
+            );
+        //construct data for carousel
+        $_carouseldata = 
+            $_products->fetchAll(
+                $_products
+                    ->select(Zend_Db_Table::SELECT_WITH_FROM_PART)->setIntegrityCheck(false)
+                    ->join('sort','sort.item_id = products.id')
+                    ->where('sort.item_type = ?','product')->where('category_id = ?', $_category_id )
+            );
+        $_designerdata = 
+            $_products->fetchAll(
+                $_products
+                    ->select(Zend_Db_Table::SELECT_WITH_FROM_PART)->setIntegrityCheck(false)
+                    ->join('sort','sort.item_id = products.id')
+                    ->where('sort.item_type = ?','product')->where('category_id = ?', $_category_id )
+                    );
+        $this->view->assign('slideshowdata', $_slideshowdata);
+        $this->view->assign('carouseldata', $_carouseldata);
+        $this->view->assign('designerdata', $_designerdata);
+        $this->render('collection');
+    }
     public function aAction() {
         $this->view->layout()->disableLayout();
         $this->view->assign('mode', $this->_getParam('m'));     // product or subcategory
