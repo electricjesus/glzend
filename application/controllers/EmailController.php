341f07c56270b708a429bb62f47d91b304b34ec7
Seth Malaki <seth.malaki@gmail.com>
2011-06-28 17:22:21 +0800

    Corrected some styles

--- application/controllers/EmailController.php
+++ application/controllers/EmailController.php
@@ -2,43 +2,38 @@
 
 class EmailController extends Zend_Controller_Action
 {
-
     public function init()
     {
         /* Initialize action controller here */
     }
-
+    
     public function indexAction()
     {
         // action body
     }
-	
-	public function sendMailAction()
-	{
-		$MailObj = new Zend_Mail();
-		$emailMessage = $this->_request->getPost('comment');
-		$fromEmail = $this->_request->getPost('email');
-		$fromFullName = $this->_request->getPost('name');
-		$to = "youngstownph@gmail.com";
-		$subject = $this->_request->getPost('subject');
-		
-		$MailObj->setBodyText($emailMessage);
-		$MailObj->setFrom($fromEmail, $fromFullName);
-		$MailObj->addTo($to);
-		$MailObj->setSubject($subject);
-		
-		
-		try
-			{
-				$MailObj->send();
-				echo "Email sent successfully";
-			}
-		catch(Zend_Mail_Exception $e)
-			{
-				echo $e;
-			}
-			
-		$this->_helper->viewRenderer->setNoRender();
-	}
+    
+    public function sendMailAction()
+    {
+        $MailObj = new Zend_Mail();
+        $emailMessage = $this->_request->getPost('comment');
+        $fromEmail = $this->_request->getPost('email');
+        $fromFullName = $this->_request->getPost('name');
+        $to = "youngstownph@gmail.com";
+        $subject = $this->_request->getPost('subject');
+        
+        $MailObj->setBodyText($emailMessage);
+        $MailObj->setFrom($fromEmail, $fromFullName);
+        $MailObj->addTo($to);
+        $MailObj->setSubject($subject);
+        
+        try {
+            $MailObj->send();
+            echo "Email sent successfully";
+        }
+        catch(Zend_Mail_Exception $e) {
+            echo $e;
+        }
+        $this->_helper->viewRenderer->setNoRender();
+    }
 }
 
