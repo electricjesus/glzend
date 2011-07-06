04164d2ce3df81c1be082a306fc1f4ddc7b47dfc
Michael Christian Young <youngstownph@gmail.com>
2011-07-06 12:45:35 +0800

    updates

--- application/controllers/EmailController.php
+++ application/controllers/EmailController.php
@@ -11,7 +11,6 @@ class EmailController extends Zend_Controller_Action
     {
         // action body
     }
-<<<<<<< HEAD
 	
 	public function sendMailAction()
 	{
@@ -31,7 +30,7 @@ class EmailController extends Zend_Controller_Action
 		try
 			{
 				$MailObj->send();
-				echo "<p class="email-success">Email sent successfully</p>";
+				echo "<p class=\"email-success\">Email sent successfully</p>";
 			}
 		catch(Zend_Mail_Exception $e)
 			{
@@ -40,31 +39,6 @@ class EmailController extends Zend_Controller_Action
 			
 		$this->_helper->viewRenderer->setNoRender();
 	}
-=======
-    
-    public function sendMailAction()
-    {
-        $MailObj = new Zend_Mail();
-        $emailMessage = $this->_request->getPost('comment');
-        $fromEmail = $this->_request->getPost('email');
-        $fromFullName = $this->_request->getPost('name');
-        $to = "youngstownph@gmail.com";
-        $subject = $this->_request->getPost('subject');
-        
-        $MailObj->setBodyText($emailMessage);
-        $MailObj->setFrom($fromEmail, $fromFullName);
-        $MailObj->addTo($to);
-        $MailObj->setSubject($subject);
-        
-        try {
-            $MailObj->send();
-            echo "Email sent successfully";
-        }
-        catch(Zend_Mail_Exception $e) {
-            echo $e;
-        }
-        $this->_helper->viewRenderer->setNoRender();
-    }
->>>>>>> 341f07c56270b708a429bb62f47d91b304b34ec7
+
 }
 
