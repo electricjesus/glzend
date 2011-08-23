6e8f68554cbd0dfd7dad45185e068b82f660e3ef
Seth Malaki <seth.malaki@gmail.com>
2011-08-23 18:15:44 +0800

    Changes to designer behaviour and style

--- public/js/custom.ajaxify.js
+++ public/js/custom.ajaxify.js
@@ -1,5 +1,6 @@
 carouselClickEvtSet = function() {
     $('a.carousel-link').click(function() {
+        clearInterval(slideshowIntervalID);
         $('#slideshow').html('');
         $('#active').removeAttr('id');
         $('#selected-product').load($(this).attr('href'))
@@ -10,15 +11,17 @@ carouselClickEvtSet = function() {
 $(document).ready(function() {
     carouselClickEvtSet.call();
     $('a.inspiration-link').click(function() {
+        if($(this).attr('id') == 'active')
+            return
         clearInterval(slideshowIntervalID);
-        slideshowIntervalID = setInterval( "slideSwitch()", 3500 );
+        //slideshowIntervalID = setInterval( "slideSwitch()", 3500 );
         $('#inspiration-active').removeAttr('id');
         $(this).attr('id','inspiration-active');
         $('div.viewport').load($(this).attr('href')+'/w/carousel', function() {
             $("div.gallery.tinycarousel").tinycarousel({ axis: 'y'});
             carouselClickEvtSet.call();
         });
-        $('#slideshow').load($(this).attr('href')+'/w/slideshow');
+        $('#slideshow').load('');
         return false;
     });
 });
