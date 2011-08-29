338f89f32cb696988db4c870650779fa39ed2b7f
Seth Malaki <electricjesus@quadsrv.(none)>
2011-08-29 22:01:17 +0800

    Added conditional for test url

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,7 +5,8 @@ class IndexController extends Zend_Controller_Action
     public function init() {
         /* Initialize action controller here */
     }
-    public function indexAction() {       
+    public function indexAction() {
+		$this->view->assign('test', $this->_getParam('test')); 
     }
     public function factoryAction() {}
     public function vacanciesAction() {}
